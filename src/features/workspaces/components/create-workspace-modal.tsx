225d1df1377245a015c2d74ac7894e1a3fa9daa9
Enea Xharja <19818937+eneax@users.noreply.github.com>
2024-10-21 15:08:15 +0200

    add toast when workspace is created

--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 import {
   Dialog,
@@ -32,6 +33,7 @@ export const CreateWorkspaceModal = () => {
       { name },
       {
         onSuccess(id) {
+          toast.success("Workspace created");
           router.push(`/workspace/${id}`);
           handleClose();
         },
